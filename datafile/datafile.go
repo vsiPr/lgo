003deb4793e1090cb8eb480aa13ed05a30437372
vsi- <90557802+vsiPr@users.noreply.github.com>
2023-02-25 22:10:58 +0200

    Update datafile.go

--- datafile/datafile.go
+++ datafile/datafile.go
@@ -12,22 +12,22 @@ func GetFile(fileName string) ([]float64, error) {
 	var numbers []float64
 	file, err := os.Open(fileName)
 	if err != nil {
-		return numbers, err
+		return nil, err
 	}
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		number, err := strconv.ParseFloat(scanner.Text(), 64)
 		if err != nil {
-			return numbers, err
+			return nil, err
 		}
 		numbers = append(numbers, number)
 	}
 	err = file.Close()
 	if err != nil {
-		return numbers, err
+		return nil, err
 	}
 	if scanner.Err() != nil {
-		return numbers, scanner.Err()
+		return nil, scanner.Err()
 	}
 	return numbers, nil
 }
