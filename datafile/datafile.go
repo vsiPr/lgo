0769de90765676e502c5a8e9af7c7482af28e13b
vsi- <90557802+vsiPr@users.noreply.github.com>
2023-02-25 21:37:20 +0200

    Update datafile.go

--- datafile/datafile.go
+++ datafile/datafile.go
@@ -8,8 +8,8 @@ import (
 )
 
 //func GetFile is used for reading float-pointing numbers from file
-func GetFile(fileName string) ([3]float64, error) {
-	var numbers [3]float64
+func GetFile(fileName string) ([]float64, error) {
+	var numbers []float64
 	file, err := os.Open(fileName)
 	if err != nil {
 		return numbers, err
@@ -17,11 +17,11 @@ func GetFile(fileName string) ([3]float64, error) {
 	i := 0
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		numbers[i], err = strconv.ParseFloat(scanner.Text(), 64)
+		number, err := strconv.ParseFloat(scanner.Text(), 64)
 		if err != nil {
 			return numbers, err
 		}
-		i++
+		numbers = append(numbers, number)
 	}
 	err = file.Close()
 	if err != nil {
